4daf149ed938b27b4823f233348f4fc1ce7981d8
artemsaledev <timmaddy01@gmail.com>
2020-01-15 16:43:05 +0200

    Stylize header, responsive menu On all breakpoints from 320 to 1920 Fix the construction of the main grid Add scripts switching classes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -6,12 +6,35 @@ $('.trigger').on('click', function() {
     $('.modal-wrapper').toggleClass('open');
     return false;
 });
+
 $('.head').on('click', function (){
     $('.modal-wrapper').removeClass('open');
 })
 
-var $easyzoom = $('.easyzoom');
- $(function () {
-    if($(window).width() > 1024) {
-    }
-  });
+$('#click-to-close').on('click', function (){
+    $('.modal-wrapper').removeClass('open');
+})
+
+if ($(window).width() < 787) {
+
+var $navButton = $('.nav-trigger');
+var $navMenu = $('.main-nav');
+var $navModal = $('.main-nav__modal');
+var $navItem = $('.nav-list__item');
+var $mainContent = $('.content-wrapper');
+
+$navButton.on("click", function() {
+  $navButton.toggleClass('nav-trigger--nav-is-visible');
+  $navModal.toggleClass('main-nav__modal--open');
+  $navMenu.toggleClass('main-nav-active');
+  $mainContent.toggleClass('main-content--nav-active');
+});
+
+$(".pagination-header a").on("click", function() {
+  $navButton.toggleClass('nav-trigger--nav-is-visible');
+  $navModal.toggleClass('main-nav__modal--open');
+  $navMenu.toggleClass('main-nav-active');
+  $mainContent.toggleClass('main-content--nav-active');
+});
+
+};
